c6ff9da0e3e3e3662dd625a364587e64644476b0
luisvicente2021 <isc.luisangel@gmail.com>
2025-12-06 14:46:09 -0500

    add register repository

---
+++ Domain/Repositories/RegisterRepository.swift
@@ -0,0 +1,11 @@
+//
+//  RegisterRepository.swift
+//  MiTiendita
+//
+//  Created by luisr on 06/12/25.
+//
+import Foundation
+
+protocol RegisterRepository {
+    func registerPerson(person: Persona)
+}
